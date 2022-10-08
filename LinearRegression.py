3f2b3c8bc88211aef7d6fab8cbde7dbc71652693
jordanotsuji <18375877+jordanotsuji@users.noreply.github.com>
2022-10-07 20:35:48 -1000

    fixed bug in compute_gradient

--- LinearRegression.py
+++ LinearRegression.py
@@ -13,13 +13,13 @@ Y = np.delete(data, np.s_[0:4], 1)
 # to a 1d array with the same # of entries 
 Y = Y.flatten()
 
-fig, ax = plt.subplots(1,4,figsize=(12,5),sharey=True)
-for i in range(len(ax)):
-    # [: x,y] syntax = row , column
-    ax[i].scatter(X[:,i], Y)
-    ax[i].set_xlabel(labels[i])
-ax[0].set_ylabel("petrol used")
-plt.show()
+# fig, ax = plt.subplots(1,4,figsize=(12,5),sharey=True)
+# for i in range(len(ax)):
+#     # [: x,y] syntax = row , column
+#     ax[i].scatter(X[:,i], Y)
+#     ax[i].set_xlabel(labels[i])
+# ax[0].set_ylabel("petrol used")
+# plt.show()
 
 """
 Thoughts after visualising dataset:
@@ -37,7 +37,7 @@ def predict(X, w, b):
 
 def compute_cost(X, y, w, b):
     """
-    calculate the cost of a predictive model with weights w and bias b
+    calculates the cost of a predictive model with weights w and bias b
     cost function used: (first image) https://math.stackexchange.com/questions/2202545/why-using-squared-distances-in-the-cost-function-linear-regression
     """
 
@@ -50,4 +50,48 @@ def compute_cost(X, y, w, b):
     totalCost /= 2*m
     return totalCost
 
+def compute_gradient(X, y, w, b):
+    """
+    computes and returns the gradient for one step of gradient descent
+    gradient function used: https://stackoverflow.com/questions/33847827/gradient-descent-for-more-than-2-theta-values 
+    """
 
+    m, n = X.shape          # m = # of examples, n = # of features in each example
+    dj_dw = np.zeros((n,))  # gradient/partial derivative of cost function in regards to w
+    dj_db = 0               # gradient/partial derivative of cost function in regards to b
+
+    for i in range(m):
+        error = (np.dot(X[i], w) + b) - y[i]
+        for j in range(n):
+            dj_dw[j] += error * X[i][j]
+        dj_db += error
+    dj_dw /= m
+    dj_db /= m
+
+    return dj_dw, dj_db
+
+X_train = np.array([[2104, 5, 1, 45], [1416, 3, 2, 40], [852, 2, 1, 35]])
+y_train = np.array([460, 232, 178])
+print(f"X Shape: {X_train.shape}, X Type:{type(X_train)})")
+print(X_train)
+print(f"y Shape: {y_train.shape}, y Type:{type(y_train)})")
+print(y_train)
+
+b_init = 785.1811367994083
+w_init = np.array([ 0.39133535, 18.75376741, -53.36032453, -26.42131618])
+print(f"w_init shape: {w_init.shape}, b_init type: {type(b_init)}")
+
+# get a row from our training data
+x_vec = X_train[0,:]
+print(f"x_vec shape {x_vec.shape}, x_vec value: {x_vec}")
+
+# make a prediction
+f_wb = predict(x_vec,w_init, b_init)
+print(f"f_wb shape {f_wb.shape}, prediction: {f_wb}")
+
+cost = compute_cost(X_train, y_train, w_init, b_init)
+print(f'Cost at optimal w : {cost}')
+
+tmp_dj_dw, tmp_dj_db= compute_gradient(X_train, y_train, w_init, b_init)
+print(f'dj_db at initial w,b: {tmp_dj_db}')
+print(f'dj_dw at initial w,b: \n {tmp_dj_dw}')
