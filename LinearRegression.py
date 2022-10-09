5d6bf4385e097f6eba7a989394eab8f56adef494
jordanotsuji <18375877+jordanotsuji@users.noreply.github.com>
2022-10-08 15:21:48 -1000

    basic regression implemented, thoughts noted

--- LinearRegression.py
+++ LinearRegression.py
@@ -1,6 +1,6 @@
 import numpy as np
 import matplotlib.pyplot as plt
-import copy
+import copy, math
 
 """
 Data Parsing/cleaning 
@@ -16,20 +16,10 @@ Y = np.delete(data, np.s_[0:4], 1)
 # flatten to make it go from a 2d array with a bunch of 1 entry arrays
 # to a 1d array with the same # of entries 
 Y = Y.flatten()
-
-"""
-test data creation
-"""
-X_train = np.array([[2104, 5, 1, 45], [1416, 3, 2, 40], [852, 2, 1, 35]])
-y_train = np.array([460, 232, 178])
-print(f"X Shape: {X_train.shape}, X Type:{type(X_train)})")
-print(X_train)
-print(f"y Shape: {y_train.shape}, y Type:{type(y_train)})")
-print(y_train)
-
-b_init = 785.1811367994083
-w_init = np.array([ 0.39133535, 18.75376741, -53.36032453, -26.42131618])
-print(f"w_init shape: {w_init.shape}, b_init type: {type(b_init)}")
+print(f"X Shape: {X.shape}, X Type:{type(X)})")
+# print(X)
+print(f"y Shape: {Y.shape}, y Type:{type(Y)})")
+# print(Y)
 
 """
 Data Plotting/Visualization
@@ -37,10 +27,24 @@ Data Plotting/Visualization
 # fig, ax = plt.subplots(1,4,figsize=(12,5),sharey=True)
 # for i in range(len(ax)):
 #     # [: x,y] syntax = row , column
-#     ax[i].scatter(X[:,i], Y)
+#     ax[i].scatter(X[:,i], Y, c="blue")
 #     ax[i].set_xlabel(labels[i])
 # ax[0].set_ylabel("petrol used")
 # plt.show()
+
+"""
+test data creation
+"""
+# X_train = np.array([[2104, 5, 1, 45], [1416, 3, 2, 40], [852, 2, 1, 35]])
+# y_train = np.array([460, 232, 178])
+# print(f"X Shape: {X_train.shape}, X Type:{type(X_train)})")
+# print(X_train)
+# print(f"y Shape: {y_train.shape}, y Type:{type(y_train)})")
+# print(y_train)
+# b_init = 785.1811367994083
+# w_init = np.array([ 0.39133535, 18.75376741, -53.36032453, -26.42131618])
+# print(f"w_init shape: {w_init.shape}, b_init type: {type(b_init)}")
+
 
 """
 Thoughts after visualising dataset:
@@ -116,6 +120,8 @@ def gradient_descent(X, y, w_in, b_in, cost_function, gradient_function, alpha, 
     b = b_in
     
     for i in range(num_iters):
+        # if(num_iters % 10 == 0):
+            # print(w)
         dj_dw, dj_db = gradient_function(X, y, w, b)
         w -= alpha * dj_dw
         b -= alpha * dj_db
@@ -123,38 +129,64 @@ def gradient_descent(X, y, w_in, b_in, cost_function, gradient_function, alpha, 
         if i < 100000:
             cost_history.append(cost_function(X, y, w, b))
 
+        if i % math.ceil(num_iters / 10) == 0:
+            print(f"Iteration {i:4d}: Cost {cost_history[-1]:8.2f}   ")
+
     return w, b, cost_history
 
 
-""" predict() test """
-# get a row from our training data
-x_vec = X_train[0,:]
-print(f"x_vec shape {x_vec.shape}, x_vec value: {x_vec}")
-# make a prediction
-f_wb = predict(x_vec,w_init, b_init)
-print(f"f_wb shape {f_wb.shape}, prediction: {f_wb}")
+# """ predict() test """
+# # get a row from our training data
+# x_vec = X_train[0,:]
+# print(f"x_vec shape {x_vec.shape}, x_vec value: {x_vec}")
+# # make a prediction
+# f_wb = predict(x_vec,w_init, b_init)
+# print(f"f_wb shape {f_wb.shape}, prediction: {f_wb}")
 
-""" compute_cost() test """
-cost = compute_cost(X_train, y_train, w_init, b_init)
-print(f'Cost at optimal w : {cost}')
+# """ compute_cost() test """
+# cost = compute_cost(X_train, y_train, w_init, b_init)
+# print(f'Cost at optimal w : {cost}')
 
-""" compute_Gradient test """
-tmp_dj_dw, tmp_dj_db= compute_gradient(X_train, y_train, w_init, b_init)
-print(f'dj_db at initial w,b: {tmp_dj_db}')
-print(f'dj_dw at initial w,b: \n {tmp_dj_dw}')
+# """ compute_Gradient test """
+# tmp_dj_dw, tmp_dj_db= compute_gradient(X_train, y_train, w_init, b_init)
+# print(f'dj_db at initial w,b: {tmp_dj_db}')
+# print(f'dj_dw at initial w,b: \n {tmp_dj_dw}')
 
-""" gradient descent testing """
-# initialize parameters
-initial_w = np.zeros_like(w_init)
-initial_b = 0.
-# some gradient descent settings
-iterations = 1000
-alpha = 5.0e-7
-# run gradient descent 
-w_final, b_final, J_hist = gradient_descent(X_train, y_train, initial_w, initial_b,
-                                                    compute_cost, compute_gradient, 
-                                                    alpha, iterations)
-print(f"b,w found by gradient descent: {b_final:0.2f},{w_final} ")
-m,_ = X_train.shape
-for i in range(m):
-    print(f"prediction: {np.dot(X_train[i], w_final) + b_final:0.2f}, target value: {y_train[i]}")
+# """ gradient descent testing """
+# # initialize parameters
+# initial_w = np.zeros_like(w_init)
+# initial_b = 0.
+# # some gradient descent settings
+# iterations = 1000
+# alpha = 5.0e-7
+# # run gradient descent 
+# w_final, b_final, J_hist = gradient_descent(X_train, y_train, initial_w, initial_b, compute_cost, compute_gradient, alpha, iterations)
+# print(f"b,w found by gradient descent: {b_final:0.2f},{w_final} ")
+# m,_ = X_train.shape
+# for i in range(m):
+#     print(f"prediction: {np.dot(X_train[i], w_final) + b_final:0.2f}, target value: {y_train[i]}")
+
+""" Regression on data """
+input_w = np.zeros(X.shape[1])
+input_b = 0
+iter = 10000
+alpha = 5.0e-9
+# initial alpyha of 5.0e-7 was too large and leading to infinite cost, 
+w_predict, b_predict, history = gradient_descent(X, Y, input_w, input_b, compute_cost, compute_gradient, alpha, iter)
+
+print(w_predict)
+for i in range(X.shape[0]):
+    print(f"prediction: {predict(X[i], w_predict, b_predict):0.2f}, target value: {Y[i]}")
+
+
+"""
+Notes after implementing regression without feature scaling or regularization:
+    At first I ran into an overflow problem which I figured was due to a large learning rate
+    and after lowering the learning rate by 100x, the cost successfully went down each iteration.
+
+    The results of the regression were a little underwhelming, with the cost reaching its lowest 
+    at around 10900, and each prediction being around 100 off of the real value. Also, the weights
+    of the features that were produced through gradient descent were not what I predicted, and seem
+    to emphasize the 2nd input rather than the 4th like I predicted. Hopefully implementing feature
+    scaling and regularization will fix this.
+"""
